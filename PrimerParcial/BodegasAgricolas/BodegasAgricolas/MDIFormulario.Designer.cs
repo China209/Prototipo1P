b1964db950618a97b91ba9940bdc156a45e322c5
CYPAMR99 <cmedinar1@miumg.edu.gt>
2020-08-25 11:40:33 -0600

    MDI Bodegas Agricolas

---
+++ PrimerParcial/BodegasAgricolas/BodegasAgricolas/MDIFormulario.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BodegasAgricolas
+{
+    partial class MDIFormulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MDIFormulario));
+            this.msMenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catalogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            this.msMenuPrincipal.SuspendLayout();
+            this.pnlPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msMenuPrincipal
+            // 
+            this.msMenuPrincipal.BackColor = System.Drawing.Color.DarkGreen;
+            this.msMenuPrincipal.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msMenuPrincipal.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.msMenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.catalogosToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.informesToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.msMenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.msMenuPrincipal.Name = "msMenuPrincipal";
+            this.msMenuPrincipal.Size = new System.Drawing.Size(1048, 31);
+            this.msMenuPrincipal.TabIndex = 0;
+            this.msMenuPrincipal.Text = "menuStrip1";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(66, 27);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // catalogosToolStripMenuItem
+            // 
+            this.catalogosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.catalogosToolStripMenuItem.Name = "catalogosToolStripMenuItem";
+            this.catalogosToolStripMenuItem.Size = new System.Drawing.Size(125, 27);
+            this.catalogosToolStripMenuItem.Text = "Catalogos";
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(108, 27);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // informesToolStripMenuItem
+            // 
+            this.informesToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            this.informesToolStripMenuItem.Size = new System.Drawing.Size(104, 27);
+            this.informesToolStripMenuItem.Text = "Informes";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(151, 27);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(88, 27);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.Controls.Add(this.picImagen);
+            this.pnlPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPrincipal.Location = new System.Drawing.Point(0, 31);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(1048, 628);
+            this.pnlPrincipal.TabIndex = 1;
+            // 
+            // picImagen
+            // 
+            this.picImagen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImagen.Image = ((System.Drawing.Image)(resources.GetObject("picImagen.Image")));
+            this.picImagen.Location = new System.Drawing.Point(0, 0);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(1048, 628);
+            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagen.TabIndex = 0;
+            this.picImagen.TabStop = false;
+            // 
+            // MDIFormulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 659);
+            this.Controls.Add(this.pnlPrincipal);
+            this.Controls.Add(this.msMenuPrincipal);
+            this.MainMenuStrip = this.msMenuPrincipal;
+            this.Name = "MDIFormulario";
+            this.Text = "Form1";
+            this.msMenuPrincipal.ResumeLayout(false);
+            this.msMenuPrincipal.PerformLayout();
+            this.pnlPrincipal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catalogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlPrincipal;
+        private System.Windows.Forms.PictureBox picImagen;
+    }
+}
+
